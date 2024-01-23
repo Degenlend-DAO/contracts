c0a674cac23db1d01559d7c7354c0e17f23638ff
Kevin Kamto <kamtokevin@gmail.com>
2024-01-22 19:26:16 -0500

    fix: degen is sent to myself

--- ignition/modules/deployDegenToken.ts
+++ ignition/modules/deployDegenToken.ts
@@ -1,7 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("Degen", (m) => {
-    const destinationAdress: string = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
+    const destinationAdress: string = "0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34";
     const degenContract = m.contract("Degen", [destinationAdress]);
 
     return {  }
