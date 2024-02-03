ce0cbab63ffeed65b94fb6155d3dc7fc9e24cbcb
Kevin Kamto <kamtokevin@gmail.com>
2024-02-03 18:55:12 -0500

    refactor: marketsare now immutable!

--- ignition/modules/cUSDC.ts
+++ ignition/modules/cUSDC.ts
@@ -10,7 +10,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("cUSDC", (m) => {
-    const cUSDC = m.contract("CErc20");
-    m.call(cUSDC, "initialize(address,address,address,uint256,string,string,uint8)", ["0x5147891461a7C81075950f8eE6384e019e39ab98", "0xd26cCEdaCb5E1166e3285ba5EF9817f45F6bfA76", "0x2CB576EcD7F0b9DEa43294E2e22E05EDc8BF2B90", 3*1e18, "Degen USDC", "cUSDC", 8])
+    const cUSDC = m.contract("CErc20Immutable", ["0x5147891461a7C81075950f8eE6384e019e39ab98", "0xd26cCEdaCb5E1166e3285ba5EF9817f45F6bfA76", "0x2CB576EcD7F0b9DEa43294E2e22E05EDc8BF2B90", 2.0e18, "Degen USDC", "cUSDC", 8, "0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34"]);
+    // m.call(cUSDC, "initialize(address,address,address,uint256,string,string,uint8)", ["0x5147891461a7C81075950f8eE6384e019e39ab98", "0xd26cCEdaCb5E1166e3285ba5EF9817f45F6bfA76", "0x2CB576EcD7F0b9DEa43294E2e22E05EDc8BF2B90", 2.0e18, "Degen USDC", "cUSDC", 8])
     return { cUSDC }
 });
--- ignition/modules/cWSX.ts
+++ ignition/modules/cWSX.ts
@@ -10,7 +10,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("cWSX", (m) => {
-    const cWSX = m.contract("CErc20");
-    m.call(cWSX, "initialize(address,address,address,uint256,string,string,uint8)", ["0x2D4e10Ee64CCF407C7F765B363348f7F62D2E06e", "0xd26cCEdaCb5E1166e3285ba5EF9817f45F6bfA76" , "0xcd24dCa1668d99ddE75AE75ecEdFf3CEEa09Fb29", 2, "Degen Wrapped SX", "cWSX", 8]);
+    const cWSX = m.contract("CErc20Immutable", ["0x2D4e10Ee64CCF407C7F765B363348f7F62D2E06e", "0xd26cCEdaCb5E1166e3285ba5EF9817f45F6bfA76" , "0xcd24dCa1668d99ddE75AE75ecEdFf3CEEa09Fb29", 2000000000000000, "Degen Wrapped SX", "cWSX", 8, "0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34"]);
+    // m.call(cWSX, "initialize(address,address,address,uint256,string,string,uint8)", ["0x2D4e10Ee64CCF407C7F765B363348f7F62D2E06e", "0xd26cCEdaCb5E1166e3285ba5EF9817f45F6bfA76" , "0xcd24dCa1668d99ddE75AE75ecEdFf3CEEa09Fb29", 2.0e18, "Degen Wrapped SX", "cWSX", 8]);
     return { cWSX }
 });
