c8d3e4466f68671a7e481e0b9efa1c9fe62e492e
Kevin Kamto <kamtokevin@gmail.com>
2024-02-03 19:23:51 -0500

    fix: return degen token details

--- ignition/modules/deployDegenToken.ts
+++ ignition/modules/deployDegenToken.ts
@@ -4,5 +4,5 @@ export default buildModule("Degen", (m) => {
     const destinationAdress: string = "0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34";
     const degenContract = m.contract("Degen", [destinationAdress]);
 
-    return {  }
+    return { degenContract }
 });
