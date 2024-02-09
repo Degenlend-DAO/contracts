889d375a65375f53c03ed220fffc06b3dc873ec4
Kevin Kamto <kamtokevin@gmail.com>
2024-02-09 08:59:41 -0500

    fix: updated comptroller

--- ignition/modules/cUSDC.ts
+++ ignition/modules/cUSDC.ts
@@ -10,7 +10,6 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("cUSDC", (m) => {
-    const cUSDC = m.contract("CErc20Immutable", ["0x5147891461a7C81075950f8eE6384e019e39ab98", "0xd26cCEdaCb5E1166e3285ba5EF9817f45F6bfA76", "0x2CB576EcD7F0b9DEa43294E2e22E05EDc8BF2B90", 2.0e18, "Degen USDC", "cUSDC", 8, "0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34"]);
-    // m.call(cUSDC, "initialize(address,address,address,uint256,string,string,uint8)", ["0x5147891461a7C81075950f8eE6384e019e39ab98", "0xd26cCEdaCb5E1166e3285ba5EF9817f45F6bfA76", "0x2CB576EcD7F0b9DEa43294E2e22E05EDc8BF2B90", 2.0e18, "Degen USDC", "cUSDC", 8])
+    const cUSDC = m.contract("CErc20Immutable", ["0x5147891461a7C81075950f8eE6384e019e39ab98", "0x8D1230e6Ae4C1Bc573697D93103349C3FDefC944", "0x2CB576EcD7F0b9DEa43294E2e22E05EDc8BF2B90", 2.0e18, "Degen USDC", "cUSDC", 8, "0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34"]);
     return { cUSDC }
 });
