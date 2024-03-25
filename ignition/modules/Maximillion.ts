9874320782be8c656da9b78df5ef312d36c42260
Kevin Kamto <kamtokevin@gmail.com>
2024-03-24 23:54:22 -0400

    chore: got rid of unnecessary modules

--- ignition/modules/Maximillion.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("Maximillion", (m) => {
-    const maximillion = m.contract("Maximillion");
-    
-    return { maximillion }
-})
