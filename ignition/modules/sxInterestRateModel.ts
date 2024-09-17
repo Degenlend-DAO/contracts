ff4051c5868e9ef0c603c64c2468bcb34ff35118
Kevin Kamto <kamtokevin@gmail.com>
2024-09-17 02:32:46 -0400

    refactor: improved interest rate models with correct params

--- ignition/modules/sxInterestRateModel.ts
+++ ignition/modules/sxInterestRateModel.ts
@@ -7,6 +7,12 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("sxInterestRateModel", (m) => {
-    const sxJumpModel = m.contract("JumpRateModelV2", [35, 15, 102, 25, "0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34"]);
-    return { sxJumpModel }
+    const sxJumpModel = m.contract("JumpRateModelV2", [
+        "350000000000000000", // Base rate (35% -> 0.35 * 1e18)
+        "150000000000000000", // Multiplier (15% -> 0.15 * 1e18)
+        "1020000000000000000", // Jump multiplier (102% -> 1.02 * 1e18)
+        "250000000000000000", // Kink (25% -> 0.25 * 1e18)
+        "0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34" // Admin address
+    ]);
+    return { sxJumpModel };
 });
--- ignition/modules/usdcInterestRateModel.ts
+++ ignition/modules/usdcInterestRateModel.ts
@@ -8,7 +8,14 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("usdcInterestRateModel", (m) => {
-    const usdcJumpModel = m.contract("JumpRateModelV2", [0, 5, 109, 80, "0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34"]);
+    // Correct the constructor values, scaling them properly to 18 decimals
+    const usdcJumpModel = m.contract("JumpRateModelV2", [
+        "0", // Base rate (0%)
+        "50000000000000000", // Multiplier (5% -> 0.05 * 1e18)
+        "1090000000000000000", // Jump multiplier (109% -> 1.09 * 1e18)
+        "800000000000000000", // Kink (80% -> 0.80 * 1e18)
+        "0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34" // Admin address (replace with correct admin address)
+    ]);
 
-    return { usdcJumpModel }
+    return { usdcJumpModel };
 });
