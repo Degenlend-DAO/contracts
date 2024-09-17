76fcf3834370006693e9b15b508c8b92b5487943
Kevin Kamto <kamtokevin@gmail.com>
2024-09-17 16:43:33 -0400

    refactor: improved contract deployments

--- ignition/modules/degenUSDC.ts
+++ ignition/modules/degenUSDC.ts
@@ -11,6 +11,18 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("degenUSDC", (m) => {
-    const cUSDC = m.contract("CErc20Immutable", ["0x5147891461a7C81075950f8eE6384e019e39ab98", "0x8D1230e6Ae4C1Bc573697D93103349C3FDefC944", "0x2CB576EcD7F0b9DEa43294E2e22E05EDc8BF2B90", 2000000000000000, "Degen USDC", "degenUSDC", 18, "0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34"]);
-    return { cUSDC }
+    const cUSDC = m.contract("CErc20Immutable", [
+        "0x5147891461a7C81075950f8eE6384e019e39ab98", // Underlying USDC address
+        "0xB078459124e55Eb9F2937c86c0Ec893ff4FF082b", // Unitroller address (proxy)
+        "0xeE1bFEE55C42048735c04A9bE7133EB62417F131", // USDC interest rate model
+        "200000000000000000000000000", // Initial exchange rate (200 * 1e18)
+        "Degen USDC", // Name
+        "degenUSDC", // Symbol
+        6, // Decimals (USDC has 6 decimals)
+        "0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34", // Fee Receiver address
+        250, // Minting fee (2.5% in basis points)
+        "0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34" // Admin address
+    ]);
+
+    return { cUSDC };
 });
--- ignition/modules/degenWSX.ts
+++ ignition/modules/degenWSX.ts
@@ -7,10 +7,24 @@
 // * symbol 
 // * decimals
 // * admin
+// * feeReciever
+// * mintFee
 
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("degenWSX", (m) => {
-    const cWSX = m.contract("CErc20Immutable", ["0x2D4e10Ee64CCF407C7F765B363348f7F62D2E06e", "0x8D1230e6Ae4C1Bc573697D93103349C3FDefC944" , "0xcd24dCa1668d99ddE75AE75ecEdFf3CEEa09Fb29", 2000000000000000, "Degen Wrapped SX", "degenWSX", 18, "0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34"]);
-    return { cWSX }
+    const cWSX = m.contract("CErc20Immutable", [
+        "0x2D4e10Ee64CCF407C7F765B363348f7F62D2E06e", // Underlying WSX address
+        "0xB078459124e55Eb9F2937c86c0Ec893ff4FF082b", // Unitroller address (proxy)
+        "0x353e7839870604Edf9D766bEFb7929c95c215e00", // WSX interest rate model
+        "200000000000000000000000000", // Initial exchange rate (200 * 1e18)
+        "Degen Wrapped SX", // Name
+        "degenWSX", // Symbol
+        18, // Decimals (WSX has 18 decimals)
+        "0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34", // Fee Receiver address
+        250, // Minting fee (2.5% in basis points)
+        "0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34" // Admin address
+    ]);
+
+    return { cWSX };
 });
