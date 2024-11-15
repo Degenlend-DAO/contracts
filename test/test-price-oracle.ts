6ed21c7896579003f41c571aa03a96cf83ed5b32
Kevin Kamto <kamtokevin@gmail.com>
2024-11-15 17:45:37 -0500

    tests: setup the price oracle for testing

--- test/test-price-oracle.ts
+++ test/test-price-oracle.ts
@@ -3,11 +3,34 @@ import { ethers } from "hardhat";
 
 
 describe("Price Oracle", () => {
-    it("should deploy the Price Oracle", async () => {
+    it("should retrieve the Simple Price Oracle", async () => {
         let priceOracle: any;
-        const PriceOracle = await ethers.getContractFactory("PriceOracle");
-        priceOracle = await PriceOracle.deploy();
 
-        expect(priceOracle.address).to.match(/^0x[0-9A-Fa-f]{40}$/);
+    before(async () => {
+        const SimplePriceOracle = await ethers.getContractAt(
+            "SimplePriceOracle",
+            "0x1f98431c8ad98523631ae4a59f267346ea31f984"    
+        );
+        expect(SimplePriceOracle.address).to.equal("0x1f98431c8ad98523631ae4a59f267346ea31f984");
     });
+});
+
+it("should list all the prices on the Price Oracle", async () => {
+    const SimplePriceOracle = await ethers.getContractAt(
+        "SimplePriceOracle",
+        "0x1f98431c8ad98523631ae4a59f267346ea31f984"
+    );
+    const prices = await SimplePriceOracle.getPrices();
+    expect(prices.length).to.be.at.least(1);
+});
+
+it("should retrive the correct price of USDC token", async () => {
+    const SimplePriceOracle = await ethers.getContractAt(
+        "SimplePriceOracle",
+        "0x1f98431c8ad98523631ae4a59f267346ea31f984"
+    );
+    const price = await SimplePriceOracle.getPrice("0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48");
+    console.log(price);
+});
+
 });
