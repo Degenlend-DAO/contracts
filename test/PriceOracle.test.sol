264315355dbdbfe05dd1197ff6e2093f3f280840
Kevin Kamto <kamtokevin@gmail.com>
2025-02-25 00:05:35 -0500

    chore: setup

--- test/PriceOracle.test.sol
+++ test/PriceOracle.test.sol
@@ -1,1 +1,15 @@
 pragma solidity ^0.8.10;
+import "../contracts/SimplePriceOracle.sol";
+import { Test } from "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+
+    contract PriceOracleTest is Test {
+        SimplePriceOracle priceOracle;
+
+        event PricePosted(address asset, uint previousPriceMantissa, uint requestedPriceMantissa, uint newPriceMantissa);
+
+        function setUp() public {
+            priceOracle = new SimplePriceOracle();
+        }
+    }
