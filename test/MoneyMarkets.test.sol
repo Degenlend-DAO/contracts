81564a298a27768c7151beeb4f5db8253e2b1171
Kevin Kamto <kamtokevin@gmail.com>
2025-03-05 22:27:31 -0500

    feat: fully functional test suite DONE

--- test/MoneyMarkets.test.sol
+++ test/MoneyMarkets.test.sol
@@ -1,9 +1,10 @@
 pragma solidity 0.8.10;
 
 import { Test } from "forge-std/Test.sol";
-import "forge-std/console.sol";
-import {stdError} from "forge-std/StdError.sol";
+import {console} from "forge-std/console.sol";
 import { ERC20 } from "../contracts/ERC20.sol";
+
+import { SimplePriceOracle } from "../contracts/SimplePriceOracle.sol";
 import { CToken } from "../contracts/libraries/money_markets/CToken.sol";
 import { CErc20Immutable } from "../contracts/libraries/money_markets/CErc20Immutable.sol";
 import { Comptroller } from "../contracts/libraries/comptroller/Comptroller.sol";
@@ -21,8 +22,21 @@ import { JumpRateModelV2 } from "../contracts/libraries/interest_rates/JumpRateM
         JumpRateModelV2 wsxInterestRate; 
         ERC20 wsx;
         ERC20 usdc;
-        address feeReceiver;
-        address admin;
+        SimplePriceOracle oracle;
+        address feeReceiver = 0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34;
+        address admin = 0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34;
+
+        address constant WSX_ADDRESS = 0x2D4e10Ee64CCF407C7F765B363348f7F62D2E06e;
+        address constant USDC_ADDRESS = 0x5147891461a7C81075950f8eE6384e019e39ab98;
+        address constant COMPTROLLER_ADDRESS = 0xB078459124e55Eb9F2937c86c0Ec893ff4FF082b;
+        address constant USDC_RATE_MODEL = 0xeE1bFEE55C42048735c04A9bE7133EB62417F131;
+        address constant WSX_RATE_MODEL = 0x353e7839870604Edf9D766bEFb7929c95c215e00;
+
+        // Deployed contract addresses
+        address constant DEGEN_USDC_ADDRESS = 0x05d225eA760bc4E974b0691bFb0Cf026A7D33279;
+        address constant DEGEN_WSX_ADDRESS = 0xD2F6594c692985850F85fd1b08d1F9DE0f719Cb5;
+
+        address me = vm.addr(vm.envUint("PRIVATE_KEY"));
 
         event Transfer(address indexed from, address indexed to, uint256 value);
    
@@ -32,62 +46,58 @@ import { JumpRateModelV2 } from "../contracts/libraries/interest_rates/JumpRateM
 
         function setUp() public {
 
-            sxNetworkFork = vm.createFork('https://rpc.toronto.sx.technology');
+            // Fork SX Testnet
+            sxNetworkFork = vm.createFork('sxTestnet');
             vm.selectFork(sxNetworkFork); // deploy on sx network
 
-            wsx = new ERC20("Wrapped SX", "WSX", 18); //0x2D4e10Ee64CCF407C7F765B363348f7F62D2E06e
-            usdc = new ERC20("USD Coin", "USDC", 6); //0x5147891461a7C81075950f8eE6384e019e39ab98
-
-            comptroller = new Comptroller(); //0xB078459124e55Eb9F2937c86c0Ec893ff4FF082b
-
-            usdcInterestRate = new JumpRateModelV2(0, 50000000000000000, 1090000000000000000, 800000000000000000, address(0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34)); //0xeE1bFEE55C42048735c04A9bE7133EB62417F131
-            wsxInterestRate = new JumpRateModelV2(350000000000000000, 150000000000000000, 1020000000000000000, 250000000000000000, address(0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34)); //0x353e7839870604Edf9D766bEFb7929c95c215e00
-            
+            wsx = ERC20(WSX_ADDRESS);
+            usdc = ERC20(USDC_ADDRESS);
+            comptroller = Comptroller(COMPTROLLER_ADDRESS);
+            usdcInterestRate = JumpRateModelV2(USDC_RATE_MODEL);
+            wsxInterestRate = JumpRateModelV2(WSX_RATE_MODEL);
             feeReceiver = 0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34;
 
-            admin = 0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34;
+            oracle = new SimplePriceOracle();
+            vm.prank(me);
+            comptroller._setPriceOracle(SimplePriceOracle(address(oracle)));
             
-            degenUSDC = new CErc20Immutable(address(usdc), // Underlying USDC address
-                comptroller, // Unitroller address (proxy)
-                usdcInterestRate, // USDC interest rate model
-                20000000000000000, // Initial exchange rate (0.02 * 1e18)
-                "Degen USDC", // Name
-                "degenUSDC", // Symbol
-                6, // Decimals (USDC has 6 decimals)
-                feeReceiver, // Fee Receiver address
-                250, // Minting fee (2.5% in basis points)
-                payable(admin) // Admin address
-            );
+            // Deploy tokens locally
+            degenUSDC = CErc20Immutable(DEGEN_USDC_ADDRESS);
 
-            
-            degenWSX = new CErc20Immutable(
-                address(wsx), // Underlying WSX address
-                comptroller, // Unitroller address (proxy)
-                wsxInterestRate, // WSX interest rate model
-                20000000000000000, // Initial exchange rate (0.02 * 1e18)
-                "Degen Wrapped SX", // Name
-                "degenWSX", // Symbol
-                18, // Decimals (WSX has 18 decimals)
-                payable(address(0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34)), // Fee Receiver address
-                250, // Minting fee (2.5% in basis points)
-                payable(address(0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34)) // Admin address
-            );
+            degenWSX = CErc20Immutable(DEGEN_WSX_ADDRESS);
 
+
+            vm.startPrank(me);
+            comptroller._supportMarket((CToken(address(degenUSDC))));
+            comptroller._supportMarket(CToken(address(degenWSX)));
+            oracle.setUnderlyingPrice(CToken(address(degenUSDC)), 1e18); // 1 USD = 1e18
+            oracle.setUnderlyingPrice(CToken(address(degenWSX)), 1e18); // Placeholder price
+            comptroller._setCollateralFactor(CToken(address(degenUSDC)), 0.5e18);
+            comptroller._setCollateralFactor(CToken(address(degenWSX)), 0.5e18);
+
+            console.log("USDC balance of me:", usdc.balanceOf(me) / 10**6, "USDC");
+            console.log("WSX balance of me:", wsx.balanceOf(me) / 10**18, "WSX");
+            vm.stopPrank();
 
         }
 
         function test_Approvals() public {
-            uint256 amount = 100;
+            vm.startPrank(me);
+            uint256 amountUSDC = 100 * 10**6; // 100 USDC
+            uint256 amountWSX = 100 * 10**18; // 100 WSX
             
             // Expect Approval event with test contract as owner.
             vm.expectEmit(true, true, false, true);
-            emit Approval(address(this), address(degenUSDC), amount);
-            usdc.approve(address(degenUSDC), amount); 
+            emit Approval(me, address(degenUSDC), amountUSDC);
+            usdc.approve(address(degenUSDC), amountUSDC); 
+
+            vm.expectEmit(true, true, false, true);
+            emit Approval(me, address(degenWSX), amountWSX);
+            wsx.approve(address(degenWSX), amountWSX);
+            vm.stopPrank();
         }
 
         function test_EnteringAMarket() public {
-
-            uint256 amount = 100;
 
             CToken[] memory assetsIn = comptroller.getAssetsIn(address(this));
 
@@ -103,34 +113,6 @@ import { JumpRateModelV2 } from "../contracts/libraries/interest_rates/JumpRateM
             }
         }
 
-        function test_WithdrawingLiquidity() public {
-            uint256 amount = 100;
-            // Expect redeem to revert (i.e. RedeemComptrollerRejection) Because, there isn't any capital in the market
-            vm.expectRevert();
-            degenWSX.redeemUnderlying(amount);
-            vm.expectRevert();
-            degenUSDC.redeemUnderlying(amount);
-
-        }
-
-        function test_BorrowingAssets() public {
-            uint256 amount = 100;
-            // Expect borrow to revert (i.e. BorrowComptrollerRejection) Because again, there isn't any capital in the market
-            vm.expectRevert();
-            degenWSX.borrow(amount);
-            vm.expectRevert();
-            degenUSDC.borrow(amount);
-        }
-
-        function test_RepayAssets() public {
-            uint256 amount = 100;
-            // Expect repay to revert (i.e. RepayBorrowComptrollerRejection)
-            vm.expectRevert();
-            degenWSX.repayBorrow(amount);
-            vm.expectRevert();
-            degenUSDC.repayBorrow(amount);
-        }
-
         function test_ExitMarkets() public {
             comptroller.exitMarket(address(degenWSX));
             comptroller.exitMarket(address(degenUSDC));
@@ -139,94 +121,87 @@ import { JumpRateModelV2 } from "../contracts/libraries/interest_rates/JumpRateM
 
         function test_IamAbleToDepositUSDSavings() public {
 
-            uint256 amount = 1000; //Let's mess around with 1,000 tokens
+            uint256 amount = 1000 * 10**6; //Let's mess around with 1,000 tokens
             address[] memory marketsUSDC = new address[](1);
             marketsUSDC[0] = address(degenUSDC);
 
-            usdc.approve(address(comptroller), amount);
+            vm.startPrank(me);
+            require(usdc.balanceOf(me) >= amount, "Insufficient USDC Balance");
+            usdc.approve(address(degenUSDC), amount);
             comptroller.enterMarkets(marketsUSDC);
             degenUSDC.mint(amount);
+            vm.stopPrank();
         
         }
 
         function test_IamAbleToDepositWSXSavings() public {
-            uint amount = 1000;
+            uint amount = 1000 * 10**18;
             address[] memory marketsWSX = new address[](1);
             marketsWSX[0] = address(degenWSX);
 
-            // Enter the market & deposit funds
-            wsx.approve(address(comptroller), amount);
+            vm.startPrank(me);
+            require(wsx.balanceOf(me) >= amount, "Insufficient WSX Balance");
+            wsx.approve(address(degenWSX), amount); // Approve CErc20
             comptroller.enterMarkets(marketsWSX);
             degenWSX.mint(amount);
-
+            vm.stopPrank();
         }
 
         function test_IfThereIsUSDSavingsICanWithdraw() public {
-            // First, check if there is USD savings IN the money market
-            uint256 amount = 1000;
+            uint256 amount = 1000 * 10**6;
 
-            // Check if there are any savings already deposited.
-            
-            if (degenUSDC.balanceOf(address(this)) > 0) {
-                degenUSDC.redeem(amount);
-            } else {
-                vm.expectRevert();
-                revert();
-            }
-
+            vm.startPrank(me);
+            usdc.approve(address(degenUSDC), amount);
+            address[] memory markets = new address[](1);
+            markets[0] = address(degenUSDC);
+            vm.startPrank(me);
+            comptroller.enterMarkets(markets);
+            degenUSDC.mint(amount); // Deposit funds
+            degenUSDC.redeemUnderlying(amount); // Should succeed
+            vm.stopPrank();
         }
 
         function test_IfThereIsWSXSavingsICanWithdraw() public {
-            // First, check if there is WSX Savings IN the money market
-
-            uint amount = 1000;
-
-            if (degenWSX.balanceOf(address(this)) > 0) {
-                degenWSX.redeem(amount);
-            } else {
-                vm.expectRevert();
-                revert();
-            }
-
+            uint256 amount = 1000 * 10**18;
+            wsx.approve(address(degenWSX), amount);
+            address[] memory markets = new address[](1);
+            markets[0] = address(degenWSX);
+            vm.startPrank(me);
+            comptroller.enterMarkets(markets);
+            degenWSX.mint(amount); // Deposit funds
+            degenWSX.redeemUnderlying(amount); // Should succeed
+            vm.stopPrank();
         }
 
         function test_IfThereIsUSDSavingsICanBorrow() public {
-            // First, check if there is USD Savings In the money market
-            uint256 amount = 100;
+            uint256 depositAmount = 1000 * 10**6;
+            uint256 borrowAmount = 100 * 10**6;
+            address[] memory markets = new address[](1);
+            markets[0] = address(degenUSDC);
 
-            if (degenUSDC.balanceOf(address(this)) > 900 )
-            {
-                degenUSDC.borrow(amount);
-            } else {
-                vm.expectRevert();
-                revert();
-            }
-
+            vm.startPrank(me);
+            require(usdc.balanceOf(me) >= depositAmount, 'insufficient USDC Balance');
+            usdc.approve(address(degenUSDC), depositAmount);
+            comptroller.enterMarkets(markets);
+            degenUSDC.mint(depositAmount); // Deposit funds
+            // comptroller._setCollateralFactor(CToken(address(degenUSDC)), 0.5e6); // Set collateral factor
+            degenUSDC.borrow(borrowAmount); // Should succeed now
+            vm.stopPrank();
         }
 
         function test_IfThereIsWSXSavingsICanBorrow() public {
-            //  First, check if there is WSX Savings IN the money market
+            uint256 depositAmount = 1000 * 10**18;
+            uint256 borrowAmount = 100 * 10**18;
+            wsx.approve(address(degenWSX), depositAmount);
+            address[] memory markets = new address[](1);
+            markets[0] = address(degenWSX);
 
-            uint256 amount = 100;
-
-            if (degenWSX.balanceOf(address(this)) > 990 )  // We will leave some dust in the account, because 2.5% of mint is transferred as fees
-            {
-                degenWSX.borrow(amount);
-            } else {
-                vm.expectRevert();
-                revert();
-            }
-
+            vm.startPrank(me);
+            comptroller.enterMarkets(markets);
+            degenWSX.mint(depositAmount); // Deposit funds
+            // comptroller._setCollateralFactor(CToken(address(degenWSX)), 0.5e18); // Set collateral factor
+            degenWSX.borrow(borrowAmount); // Should succeed now
+            vm.stopPrank();
         }
 
-        function test_IfThereIsUSDSavingsICanRepay() public {
-            // First, check if there is WSX debt I have to repay
-            
-            
-        }
-
-        function test_IfThereIsWSXSavingsICanRepay() public {
-            // First, check if there is WSX debt I have to repay
-
-        }
     }
