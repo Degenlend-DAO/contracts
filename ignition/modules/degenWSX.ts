291f2bba4892cb05afd9b8b29429d25c22e18799
Kevin Kamto <kamtokevin@gmail.com>
2025-05-02 12:52:49 -0400

    refactor: tackling exchange rate mantissa & decimals for normalization

--- ignition/modules/degenWSX.ts
+++ ignition/modules/degenWSX.ts
@@ -17,10 +17,10 @@ export default buildModule("degenWSX", (m) => {
         "0x2D4e10Ee64CCF407C7F765B363348f7F62D2E06e", // Underlying WSX address
         "0xB078459124e55Eb9F2937c86c0Ec893ff4FF082b", // Unitroller address (proxy)
         "0x353e7839870604Edf9D766bEFb7929c95c215e00", // WSX interest rate model
-        "20000000000000000", // Initial exchange rate (0.02 * 1e18)
+        "2000000000000000000000000000", // ✅ 2e28 (18 dec underly, 8 dec cToken)
         "Degen Wrapped SX", // Name
         "degenWSX", // Symbol
-        18, // Decimals (WSX has 18 decimals)
+        8, // 8 Decimals (WSX has 18 decimals) is standard for cTokens
         "0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34", // Fee Receiver address
         250, // Minting fee (2.5% in basis points)
         "0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34" // Admin address
