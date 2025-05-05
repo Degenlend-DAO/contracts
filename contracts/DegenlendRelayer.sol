34df048cfe0e0d895ececcff4a9d4c1b6668499e
Kevin Kamto <kamtokevin@gmail.com>
2025-05-04 20:48:06 -0400

    refactor: flushed out relayer to handle intents

--- contracts/DegenlendRelayer.sol
+++ contracts/DegenlendRelayer.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 interface ICERC20 {
     function mint(uint mintAmount) external returns (uint);
@@ -11,20 +12,21 @@ interface ICERC20 {
     function borrow(uint borrowAmount) external returns (uint);
     function repayBorrow(uint repayAmount) external returns (uint);
     function underlying() external view returns (address);
+    function underlying() external view returns (address);
 }
 
 contract DegenLendRelayer is EIP712 {
+contract DegenLendRelayer is EIP712 {
     using ECDSA for bytes32;
 
-    // Typehashes for EIP-712 signatures
     bytes32 public constant MINT_TYPEHASH = 
         keccak256("MintIntent(address user,address cToken,uint256 amount,uint256 nonce,uint256 deadline)");
     bytes32 public constant REDEEM_TYPEHASH = 
         keccak256("RedeemIntent(address user,address cToken,uint256 amount,uint256 nonce,uint256 deadline)");
     bytes32 public constant BORROW_TYPEHASH = 
-        keccak256("BorrowIntent(address user,address cToken,uint256 amount,uint256 nonce,uint256 deadline)");
+        keccak256("BorrowIntent(address user, address cToken,uint256 amount, uint256 nonce,uint256 deadline)");
     bytes32 public constant REPAY_TYPEHASH = 
-        keccak256("RepayIntent(address user,address cToken,uint256 amount,uint256 nonce,uint256 deadline)");
+        keccak256("RepayIntent(address user, address cToken, uint256 amount, uint256 nonce, uint256 deadline)");
 
     mapping(address => uint256) public nonces;
 
